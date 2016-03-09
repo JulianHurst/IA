1e2c4582b61e9737be3333ba5f8492c7aaff1872
Guest <guest-CNZmke@ESJFST-S314L15.salsa.univ-amu.fr>
2016-03-09 17:59:51 +0100

    Modif dpll ajout fichier test

--- dpll.c
+++ dpll.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /* a = 1
  * -a=2
@@ -10,7 +11,7 @@
  // etat_var combien de valeurs essayées pr une variable (vecteur 1D à 0 au début)
  // longeueur clause = 2 au départ
  // etat_clause
- 
+ /*
  typedef struct{
 	 int val;
 	 lit *suiv
@@ -19,21 +20,40 @@
  typedef struct{
 	 
  }clause;
- 
- void readfic(char *file,){
+ */
+ void readfic(char *file,char **C){
 	 FILE *fp;
-	 int ch;
+	 int ch,i,j=0,size=0;
 	 fp=fopen(file,"r");
 	 if(fp){
-		 while(ch=getc(fp)!=EOF){
-			 
+		while(ch=fgetc(fp)!=EOF){
+			printf("%c\n",ch);
+			if(ch=='\n')
+				size++;
+		}
+		printf("hi %d\n",size);
+		rewind(fp);
+		C=malloc(sizeof(char*)*5);
+		for(i=0;i<size;i++)
+			C[i]=malloc(sizeof(char*)*size);		
+		while(ch=fgetc(fp)!=EOF){
+			while(ch!='\n'){
+				if(ch!=' '){
+					C[i][j]=ch;
+					j++;
+				}
+			} 
+			i++;
 		 }
+		 fclose(fp);
+	 }
  }
  
- int dpll(){
-	 
- }
+ 
+ int dpll(){}
  
 int main(int argc,char **argv){
+	char **C;
 	
+	readfic(argv[1],C);	
 }
