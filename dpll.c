dc143538af58cf8f18f77a1782e1df8de54aee03
JulianHurst <julian.hurst@laposte.net>
2016-03-13 11:32:52 +0100

    readfic qui marche et ajout print

--- dpll.c
+++ dpll.c
@@ -7,53 +7,133 @@
  * -b=4
  * ...
  */
- 
+
  // etat_var combien de valeurs essayées pr une variable (vecteur 1D à 0 au début)
  // longeueur clause = 2 au départ
  // etat_clause
+
  /*
  typedef struct{
 	 int val;
 	 lit *suiv
  }lit;
- 
+
  typedef struct{
-	 
+
  }clause;
  */
- void readfic(char *file,char **C){
+
+typedef struct{
+  int size;
+  char **C;
+}clauselit;
+
+typedef struct{
+  int size;
+  int **C;
+}clause;
+
+
+//lit les clauses dans un tableau 2D de CHAR
+ void readficlit(char *file,clauselit *cl){
 	 FILE *fp;
 	 int ch,i,j=0,size=0;
 	 fp=fopen(file,"r");
 	 if(fp){
-		while(ch=fgetc(fp)!=EOF){
-			printf("%c\n",ch);
+		while((ch=fgetc(fp))!=EOF){
 			if(ch=='\n')
 				size++;
 		}
-		printf("hi %d\n",size);
+		printf("size : %d\n",size);
 		rewind(fp);
-		C=malloc(sizeof(char*)*5);
+    cl->size=size;
+		cl->C=malloc(sizeof(char*)*size+1);
 		for(i=0;i<size;i++)
-			C[i]=malloc(sizeof(char*)*size);		
-		while(ch=fgetc(fp)!=EOF){
+			cl->C[i]=malloc(sizeof(char*)*size);
+    i=0;
+		while((ch=fgetc(fp))!=EOF){
 			while(ch!='\n'){
 				if(ch!=' '){
-					C[i][j]=ch;
+					cl->C[i][j]=ch;
 					j++;
 				}
-			} 
+        ch=fgetc(fp);
+			}
+      cl->C[i][j]='\0';
+      j=0;
 			i++;
 		 }
 		 fclose(fp);
 	 }
  }
- 
- 
+
+//lit les clauses dans un tableau 2D de INT selon où a=1 , -a=2 , b=3 ...
+ void readfic(char *file,clause *cl){
+   FILE *fp;
+	 int ch,i,j=0,size=0;
+	 fp=fopen(file,"r");
+	 if(fp){
+		while((ch=fgetc(fp))!=EOF){
+			if(ch=='\n')
+				size++;
+		}
+    printf("size : %d\n",size);
+		rewind(fp);
+    cl->size=size;
+		cl->C=malloc(sizeof(int*)*size+1);
+		for(i=0;i<size;i++)
+			cl->C[i]=malloc(sizeof(int*)*size);
+    i=0;
+		while((ch=fgetc(fp))!=EOF){
+			while(ch!='\n'){
+				if(ch=='-'){
+          ch=fgetc(fp);
+          cl->C[i][j]=(int)ch-96;
+          cl->C[i][j]=cl->C[i][j]*2;
+          j++;
+        }
+        else if(ch!=' '){
+					cl->C[i][j]=(int)ch-96;
+          cl->C[i][j]=cl->C[i][j]*2-1;
+					j++;
+				}
+        ch=fgetc(fp);
+			}
+      cl->C[i][j]=0;
+      j=0;
+			i++;
+		 }
+		 fclose(fp);
+	 }
+ }
+
+//affiche les clauses pour la structure avec tableau de CHARR
+ void printclauseslit(clauselit cl){
+  for(int i=0;i<cl.size;i++){
+    for(int j=0;cl.C[i][j]!='\0';j++)
+      printf("%c ",cl.C[i][j]);
+    printf("\n");
+  }
+ }
+
+//affiche les clauses pour la structure avec tableau de INT
+ void printclauses(clause cl){
+  for(int i=0;i<cl.size;i++){
+    for(int j=0;cl.C[i][j]!=0;j++)
+      printf("%d ",cl.C[i][j]);
+    printf("\n");
+  }
+ }
+
+
  int dpll(){}
- 
+
 int main(int argc,char **argv){
-	char **C;
-	
-	readfic(argv[1],C);	
+  clause *cl;
+  cl=malloc(sizeof(clause));
+  //clauselit *cl;
+  //cl=malloc(sizeof(clauselit));
+
+	readfic(argv[1],cl);
+  printclauses(*cl);
 }
