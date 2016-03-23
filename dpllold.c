c31373a1d365864718b987b688ad90fb995a946a
Julian Hurst <julian.hurst@laposte.net>
2016-03-23 14:21:09 +0100

    Renommage

--- dpllold.c
+++ dpllold.c
@@ -135,7 +135,7 @@ typedef struct{
     printf("\n");
   }
  }
-
+ 
 //Affiche les clauses pour la structure avec tableau de INT en CHAR
 void printclausesch(clause cl){
   for(int i=0;i<cl.size;i++){
@@ -166,7 +166,7 @@ int findcl(int x,clause cl){
   return 0;
 
 }
-/*
+
 //Renvoie 1 s'il y a une inconsistence dans l'ensemble de clauses (pas satisfiable)
 int inconsistent(clause cl, lit taken){
   //int n=max(cl);
@@ -174,36 +174,17 @@ int inconsistent(clause cl, lit taken){
     if(cl.C[i].size==1){
       if(!(cl.C[i].l[0]%2)){
         if(findlit(cl.C[i].l[0]-1,taken)!=-1)
-    			return 1;
-    		for(int j=i+1;j<cl.size;j++)
-    			if(cl.C[j].size==1 && cl.C[i].l[0]-1==cl.C[j].l[0])
-    				return 1;
+			return 1;
+		for(int j=i+1;j<cl.size;j++)
+			if(cl.C[j].size==1 && cl.C[i].l[0]-1==cl.C[j].l[0])
+				return 1;
       }
       else{
         if(findlit(cl.C[i].l[0]+1,taken)!=-1)
           return 1;
           for(int j=i+1;j<cl.size;j++)
-      			if(cl.C[j].size==1 && cl.C[i].l[0]+1==cl.C[j].l[0])
-      				return 1;
-	  }
-	}
-  return 0;
-}
-*/
-//Renvoie 1 s'il y a une inconsistence dans l'ensemble de clauses (pas satisfiable)
-int inconsistent(clause cl){
-  //int n=max(cl);
-  for(int i=0;i<cl.size;i++)
-    if(cl.C[i].size==1){
-      if(!(cl.C[i].l[0]%2)){
-    		for(int j=i+1;j<cl.size;j++)
-    			if(cl.C[j].size==1 && cl.C[i].l[0]-1==cl.C[j].l[0])
-    				return 1;
-      }
-      else{
-          for(int j=i+1;j<cl.size;j++)
-      			if(cl.C[j].size==1 && cl.C[i].l[0]+1==cl.C[j].l[0])
-      				return 1;
+			if(cl.C[j].size==1 && cl.C[i].l[0]+1==cl.C[j].l[0])
+				return 1;
 	  }
 	}
   return 0;
@@ -283,13 +264,13 @@ int dpll(clause *cl){
   //Si vide renvoie vrai
   while(cl->size!=0){
 	  // si pb renvoie faux
-	  if(inconsistent(*cl))
-		  return 0;
+	  if(inconsistent(*cl,taken))
+		return 0;
 	  //vérifie mono-littéraux
 	  if((l=monolit(*cl)));
 	  else if((l=veriflitpurs(*cl)));
 	  else
-		  l=cl->C[0].l[0];
+		l=cl->C[0].l[0];
 	  taken.l[i]=l;
 	  taken.size++;
 	  if(l%2==0)
@@ -308,138 +289,9 @@ int dpll(clause *cl){
   return 1;
 }
 
-int all(clause cl,lit taken_all){
-  for(int i=0;i<cl.size;i++)
-    for(int j=0;j<cl.C[i].size;j++)
-      if(findlit(cl.C[i].l[j],taken_all)==-1)
-        return 0;
-  return 1;
-}
-
-int chooseunsignedlit(clause cl){
-  for(int i=0;i<cl.size;i++)
-    for(int j=0;j<cl.C[i].size;j++)
-      if((cl.C[i].l[j]%2)!=0)
-        return cl.C[i].l[j];
-  return 0;
-}
-
-int back(lit takenrand){
-  for(int i=takenrand.size-1;i>=0;i--){
-    if(takenrand.l[i]%2==0){
-      if(findlit(takenrand.l[i]-1,takenrand)==-1)
-        return takenrand.l[i]-1;
-      }
-      else{
-        if(findlit(takenrand.l[i]+1,takenrand)==-1)
-          return takenrand.l[i]+1;
-      }
-  }
-  return 0;
-}
-
-clause * rollback(clause *cl, lit taken, int b){
-	for(int i=0;i<b;i++)
-	  cl=choicelit(taken.l[i],cl);
-	return cl;
-}
-
-clause * copy(clause *cl){
-	clause *tmp;
-	tmp=malloc(sizeof(clause *));
-	tmp->C=malloc(sizeof(lit*)*(cl->size*2));
-	tmp->size=cl->size;
-	for(int i=0;i<cl->size;i++)
-		  tmp->C[i].l=malloc(sizeof(int*)*50);
-	for(int i=0;i<cl->size;i++)
-		for(int j=0;j<cl->C[i].size;j++){
-			tmp->C[i].size=cl->C[i].size;
-			tmp->C[i].l[j]=cl->C[i].l[j];
-		}
-	return tmp;
-}
-
-clause * stupidcopy(clause *cl){
-	return cl;
-}
-
-/*taken_all contains all taken litterals since start of dpll
-  taken contains taken litterals for a given path in dpll
-  check taken for pure litteral
-  delete all taken litterals after pure litteral from taken
-  take opposite polarity of litteral or next litteral
-  repeat till taken_all contains all litterals
-*/
-
-//takenrand = litts taken randomly
-
-//Applique dpll sur un ensemble de clauses
-int dpll_all(clause *cl){
-  int i=0,l,random=0,b;
-  lit taken_all,taken,takenrand;
-  clause *tmp;
-  tmp=copy(cl);
-  taken.size=0;
-  taken.l=malloc(sizeof(int*)*100);
-    takenrand.size=0;
-  takenrand.l=malloc(sizeof(int*)*100);
-  //Si vide renvoie vrai
-  //REPLACE WITH something ! Maybe while pure lit exists in takenrand maybe same as above 
-  while(cl->size!=0){
-      l=0;
-	  // si pb renvoie faux
-	  if(inconsistent(*cl)){
-      if(random){
-        if((l=back(takenrand))==0)  //go back to last pure litteral (from back to front) taken randomly
-          return 0;
-        else{
-          if(l%2==0)
-            b=findlit(l-1,taken);
-          else
-            b=findlit(l+1,taken);
-          taken.l[b]=l;
-          cl=rollback(tmp,taken,b);  //rolls back changes on cl to moment b (using all taken litterals up to b, even unitary or pure)
-          //maybe resize takenrand
-      }
-      }
-      else
-		    return 0;
-    }
-      if(l==0){
-	  //vérifie mono-littéraux ou lit purs
-	  if((l=monolit(*cl)));
-	  else if((l=veriflitpurs(*cl)));
-	  else{
-      	random=1;
-		  l=chooseunsignedlit(*cl);
-      	takenrand.l[i]=l;
-      	takenrand.size++;
-    }
-    }
-	  taken.l[i]=l;
-    taken.size++;
-    //taken_all.l[i]=l;
-    //taken_all.size++;
-	  if(l%2==0)
-		  printf("l : -%c\n",l/2+96);
-    else
-		  printf("l : %c\n",(l+1)/2+96);
-	  cl=choicelit(l,cl);
-	  printclausesch(*cl);
-	  if(cl->size==0)
-		  printf("ensemble vide\n\n");
-	  else
-		  printf("\n");
-		//vérification littéraux
-		i++;
-  }
-  return 1;
-}
-
 int main(int argc,char **argv){
   int ret=0;
   clause *cl;
-  clause *tmp;
   /*
   lit li;
   li.l=malloc(sizeof(int*)*100);
@@ -451,24 +303,18 @@ int main(int argc,char **argv){
 	fprintf(stderr,"USAGE : %s fichier.sat\n",argv[0]);
 	return -1;
   }
-
+	
   cl=readfic(argv[1]);
   //printclauses(*cl);
-  tmp=copy(cl);
-  //cl=choicelit(1,cl);
-  //printclausesch(*cl);
-  //printf("Salut\n");
-  //printclausesch(*tmp);
-  
   printclausesch(*cl);
   printf("\n");
-
-  if((ret=dpll_all(cl)))
+  
+  if((ret=dpll(cl)))
 	printf("Satisfiable\n");
   else
 	printf("Non satisfiable\n");
   return ret;
-
+  
   /* TESTS Fonctionnels
   cl=choicelit(34,cl);
   //cl=choicelit(31,cl);
@@ -479,5 +325,5 @@ int main(int argc,char **argv){
   printf("%d\n",inconsistent(*cl,li));
   printf("%d\n",monolit(*cl));
   printf("%d\n",veriflitpurs(*cl));*/
-
+   
 }
